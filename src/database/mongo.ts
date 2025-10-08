74018cbe13e7f73074dd5427690d055220c8390b
Aldlanga80 <aldo.d.langa@gmail.com>
2025-10-07 21:35:54 -0300

    añadido de .env

---
+++ src/database/mongo.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ quiet: true });
+
+const uri = process.env.MONGO_URI || "mongodb://localhost:27017/mi_base";
+
+mongoose.connect(uri)
+  .then(() => console.log("✅ Conectado a MongoDB correctamente"))
+  .catch((err) => console.error("❌ Error al conectar con MongoDB:", err));
